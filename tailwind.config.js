64e19df34bf8b1b02a8d3f0d0836b944c0fd382c
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-04-12 21:11:59 +0200

    🍱 🔧 🎉 Add assets and config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: { 'food-image': "url'/src/assets/logoQuickDeli.png')", 'food-image-2': "url('/src/assets/bgfood.png')"},
+    },
   },
   plugins: [],
 }
