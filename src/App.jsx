a82b496966f23f3c43b3da3085e825c1648e7a09
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-04-16 17:06:48 +0200

    ✨ F02: Creating Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/header/Navbar.jsx";
+import Hero from "./components/hero/Hero.jsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
